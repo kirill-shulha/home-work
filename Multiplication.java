9e8b665f8ac148ac5ca7032bc43db005fe362437
kirill-shulha <strawberrydeejay@gmail.com>
2022-08-09 19:19:11 +0300

    did homework lesson 3

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+public class Multiplication {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        for (int i = 1; i <= 10; i++) {
+            System.out.println();
+            for (int j = 2; j <= 10; j++) {
+                System.out.print(i * j + " ");
+
+            }
+        }
+    }
+}
