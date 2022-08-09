348c85b536e4d9ad5420da8ba1f54c18cbad104b
kirill-shulha <strawberrydeejay@gmail.com>
2022-08-09 23:41:49 +0300

    did homework lesson 3

---
+++ SequenceOfNumbers.java
@@ -0,0 +1,10 @@
+public class SequenceOfNumbers {
+    public static void main(String[] args){
+        for (int i=0; i<=50; i++){
+            if (i%10 != 2){
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+}
